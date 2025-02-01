92e9647a4ab27dbc1acce7f2ac6e4a02e22c9155
Arib61 <153956244+Arib61@users.noreply.github.com>
2025-02-01 11:03:40 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -185,9 +185,10 @@ for key, default_path in default_files.items():
         st.markdown(f"[📥 Télécharger {key.capitalize()} par défaut]({FLASK_BASE_URL}/default-excel/{default_filename})", unsafe_allow_html=True)
 
 # 📥 Conversion en XML en prenant en compte les fichiers sélectionnés ou par défaut
+st.markdown('<p class="sub-title">🛠️ Convertir les fichiers en XMLL</p>', unsafe_allow_html=True)
 if st.button("📤 Convertir les fichiers en XML"):
     for key, default_path in default_files.items():
-        save_path = f"data_excel/{key.capitalize()}_GINF2.xlsx"
+        save_path = f"../../data_excel/{key.capitalize()}_GINF2.xlsx"
 
         if uploaded_files[key]:  # Vérifier si un fichier a été chargé ou sélectionné par défaut
             os.makedirs(os.path.dirname(save_path), exist_ok=True)
